598bee7dafb0dfe3bbd332322b09d5aaf261b9b8
styx11 <1654630248@qq.com>
2021-03-12 16:06:00 +0800

    update main

--- views/src/main.ts
+++ views/src/main.ts
@@ -1,14 +1,27 @@
-import { createApp } from 'vue'
+import { createApp, App } from 'vue'
 
-import App from './App.vue'
+import AppComponent from './App.vue'
+import MeshIcon from './components/Icon.vue'
 import store from './store'
 import router from './router'
 
 const initVueApp = () =>
 {
-	createApp(App)
+	const app = createApp(AppComponent)
 		.use(store)
 		.use(router)
-		.mount('#app')
+
+	registerSvg(app)
+
+	app.mount('#app')
 }
 initVueApp()
+
+function registerSvg(app: App)
+{
+	app.component('Icon', MeshIcon)
+	// 使用require.context自动导入svg文件
+	const requireAll = requireContext => requireContext.keys().map(requireContext)
+	const req = require.context('./static/svg/', true, /\.svg$/)
+	requireAll(req)
+}
