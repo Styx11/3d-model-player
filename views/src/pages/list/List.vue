8d0dff96923371974579a25da3480557493b8998
styx11 <1654630248@qq.com>
2021-03-23 11:17:53 +0800

    feat: update List layout

--- views/src/pages/list/List.vue
+++ views/src/pages/list/List.vue
@@ -6,7 +6,9 @@
 				<header class="main_header">
 					<Tags :tags="tags" v-model:checkedTag="checkedTag" />
 				</header>
-				<main class="main_content"></main>
+				<main class="main_content">
+					<Cards />
+				</main>
 			</section>
 		</section>
 	</div>
@@ -17,12 +19,14 @@
 	import { Button } from 'ant-design-vue'
 
 	import Tags from './components/Tags.vue'
+	import Cards from './components/Cards.vue'
 	import TitleBar from '@/components/TitleBar'
 
 	export default defineComponent({
 		name: 'List',
 		components: {
 			Tags,
+			Cards,
 			Button,
 			TitleBar,
 		},
@@ -30,6 +34,8 @@
 		{
 			const checkedTag = ref<string>('')
 			const tags = ref<Array<string>>([...new Set(['sdasd', 'taasasf', 'asd', 'sdad', 'sda', 'tasasf', 'asdsd'])])
+
+			watch(checkedTag, newVal => console.log(newVal))
 
 			return {
 				tags,
@@ -53,7 +59,9 @@
 		height: 100vh;
 		display: flex;
 		flex-direction: column;
+		overflow: hidden;
 		.list_main_container {
+			height: 100%;
 			flex: 1 0 auto;
 			border-top: 1px solid @BLUE0;
 			background: @BLUE1;
@@ -65,12 +73,14 @@
 				.main_header {
 					width: 100%;
 					height: 141px;
+					flex: 1 0 auto;
 					overflow: auto;
 					.scrollBar(6px);
 				}
 				.main_content {
 					height: 100%;
 					width: 100%;
+					overflow: hidden;
 				}
 			}
 		}
